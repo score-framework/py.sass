899a6024f9c01a3f93cfb7f99ffbfffb35d41a0e
Necdet Can Ateşman <can@strg.at>
2017-05-15 14:26:25 +0200

    Release: 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.sass',
-    version='0.0.1',
+    version='0.0.2',
     description='Sass integration for The SCORE Framework',
     long_description=README,
     author='strg.at',
